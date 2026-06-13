6556bf52fdd3bf5b13232175d61d8a2953211cf4
kamilio-pitoni <104303595+kamilio-pitoni@users.noreply.github.com>
2026-06-13 16:48:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import os
-import sys
 import time
 import telebot
 
-# Бот берёт токен из настроек самого хостинга (Переменные окружения)
-TOKEN = os.getenv("7210536235:AAGCl2u-WTpZpuLQxvj60sxZgZdePivBG2g")
-
-if not TOKEN:
-    print(
-        "Ошибка: Переменная BOT_TOKEN не найдена в настройках хостинга!",
-        file=sys.stderr,
-    )
-    sys.exit(1)
+# Твой тестовый токен зашит прямо в код
+TOKEN = "7210536235:AAGCl2u-WTpZpuLQxvj60sxZgZdePivBG2g"
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -20,18 +11,22 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_types=["text"])
 def echo_all(message):
     try:
+        # Бот просто отправляет обратно тот же текст, что ты ему написал
         bot.reply_to(message, message.text)
     except Exception as e:
         print(f"Ошибка при отправке сообщения: {e}")
 
 
-# Главный конвейер запуска
+# Главный конвейер запуска, который защищает от падений
 if __name__ == "__main__":
     print("Бот успешно запущен и готов к работе...")
 
     while True:
         try:
+            # Настройки для стабильного удержания соединения
             bot.polling(none_stop=True, interval=0, timeout=20)
         except Exception as error:
+            # Если оборвется сеть, бот не упадет, а перезапустится через 5 секунд
             print(f"Произошел сбой сети: {error}. Перезапуск через 5 секунд...")
             time.sleep(5)
+            
